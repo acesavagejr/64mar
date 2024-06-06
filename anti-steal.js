5f291ffb39f435ccda35299a390ac07111212564
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-06-05 18:44:37 -0700

    Add files via upload

---
+++ anti-steal.js
@@ -0,0 +1,7 @@
+const container = document.getElementById('container');
+
+if (container.textContent.includes('SM64-JS | Made by Chase Wicklund on replit! (@jscraft)')) {
+  console.log('');
+} else {
+  console.log('Do not steal code. Here is the original repl. https://replit.com/@jscraft/sm64');
+}
