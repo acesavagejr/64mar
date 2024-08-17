695f26a51a33b6add970edcd43d699f3dae4b2d7
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-08-16 18:38:42 -0600

    Update anti-steal.js

--- anti-steal.js
+++ anti-steal.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container');
 
-if (container.textContent.includes('SM64-JS | Made by Chase Wicklund on replit! (@jscraft)')) {
+if (container.textContent.includes('Hello :)')) {
   console.log('');
 } else {
   console.log('Do not steal code. Here is the original repl. https://replit.com/@jscraft/sm64');
